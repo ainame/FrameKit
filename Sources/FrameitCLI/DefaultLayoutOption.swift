dbdf8fe1445060f53331a09f0c9452bb9651619a
ainame <s.namai.09@gmail.com>
2020-09-13 22:07:34 +0100

    Add cases to DefaultLayoutOption

--- Sources/FrameitCLI/DefaultLayoutOption.swift
+++ Sources/FrameitCLI/DefaultLayoutOption.swift
@@ -5,10 +5,13 @@ import FrameitLib
 
 enum DefaultLayoutOption: String, RawRepresentable, ExpressibleByArgument, LayoutProviderOption {
     case iPhone65 = "iphone_65"
-    case iPhone65Hero = "iphone_65_hero"
     case iPhone55 = "iphone_55"
     case iPadPro = "ipad_pro"
     case iPadPro3rdGen = "ipad_pro_3rd_gen"
+    case iPhone65Hero = "iphone_65_hero"
+    case iPhone55Hero = "iphone_55_hero"
+    case iPadProHero = "ipad_pro_hero"
+    case iPadPro3rdGenHero = "ipad_pro_3rd_gen_hero"
 
     public init?(argument: String) {
         self.init(rawValue: argument)
@@ -16,11 +19,14 @@ enum DefaultLayoutOption: String, RawRepresentable, ExpressibleByArgument, Layou
 
     public var value: DefaultLayout {
         switch self {
-        case .iPadPro: return DefaultLayout.iPadPro
-        case .iPadPro3rdGen: return DefaultLayout.iPadPro3rdGen
-        case .iPhone55: return DefaultLayout.iPhone55
-        case .iPhone65: return DefaultLayout.iPhone65
-        case .iPhone65Hero: return DefaultLayout.iPhone65Hero
+        case .iPadPro: return .iPadPro
+        case .iPadPro3rdGen: return .iPadPro3rdGen
+        case .iPhone55: return .iPhone55
+        case .iPhone65: return .iPhone65
+        case .iPhone65Hero: return .iPhone65Hero
+        case .iPadPro3rdGenHero: return .iPadPro3rdGenHero
+        case .iPadProHero: return .iPadProHero
+        case .iPhone55Hero: return .iPhone55Hero
         }
     }
 }
