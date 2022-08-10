2a44f82d229f4a101263a52e04b23b273cfb934e
ainame <s.namai.09@gmail.com>
2022-08-10 14:41:44 +0100

    Fix warnings

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "FrameKitCLI",
             dependencies: [
                 .target(name: "FrameKit"),
@@ -38,7 +38,7 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
-        .target(
+        .executableTarget(
             name: "FrameKitTemplateCLI",
             dependencies: [
                 .target(name: "FrameKit"),
