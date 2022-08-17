4e76182b6d6d5fd13009d86ff6837ada31c70aff
ainame <s.namai.09@gmail.com>
2022-08-17 09:48:48 +0100

    Remove useless landscape layout

--- Sources/FrameKitCLI/SampleLayoutOption.swift
+++ Sources/FrameKitCLI/SampleLayoutOption.swift
@@ -12,15 +12,6 @@ public enum SampleLayoutOption: String, RawRepresentable, ExpressibleByArgument,
     case iPadProHero = "ipad_pro_hero"
     case iPadPro3rdGenHero = "ipad_pro_3rd_gen_hero"
 
-    case iPhone65Landscape = "iphone_65_landscape"
-    case iPhone55Landscape = "iphone_55_landscape"
-    case iPadProLandscape = "ipad_pro_landscape"
-    case iPadPro3rdGenLandscape = "ipad_pro_3rd_gen_landscape"
-    case iPhone65HeroLandscape = "iphone_65_hero_landscape"
-    case iPhone55HeroLandscape = "iphone_55_hero_landscape"
-    case iPadProHeroLandscape = "ipad_pro_hero_landscape"
-    case iPadPro3rdGenHeroLandscape = "ipad_pro_3rd_gen_hero_landscape"
-
     public init?(argument: String) {
         self.init(rawValue: argument)
     }
@@ -35,14 +26,6 @@ public enum SampleLayoutOption: String, RawRepresentable, ExpressibleByArgument,
         case .iPadPro3rdGenHero: return .iPadPro3rdGenHero
         case .iPadProHero: return .iPadProHero
         case .iPhone55Hero: return .iPhone55Hero
-        case .iPadProLandscape: return .iPadProLandscape
-        case .iPadPro3rdGenLandscape: return .iPadPro3rdGenLandscape
-        case .iPhone55Landscape: return .iPhone55Landscape
-        case .iPhone65Landscape: return .iPhone65Landscape
-        case .iPhone65HeroLandscape: return .iPhone65HeroLandscape
-        case .iPadPro3rdGenHeroLandscape: return .iPadPro3rdGenHeroLandscape
-        case .iPadProHeroLandscape: return .iPadProHeroLandscape
-        case .iPhone55HeroLandscape: return .iPhone55HeroLandscape
         }
     }
 }
