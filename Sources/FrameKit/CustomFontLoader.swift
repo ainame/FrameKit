3359872fe0a7ffcbab60855807448f1f397590a6
ainame <s.namai.09@gmail.com>
2022-08-18 11:33:16 +0100

    Register Font name when registering font file

--- Sources/FrameKit/CustomFontLoader.swift
+++ Sources/FrameKit/CustomFontLoader.swift
@@ -2,23 +2,29 @@ import Foundation
 import CoreText
 
 public class CustomFontLoader {
+    public struct FontEntry: Hashable {
+        let fileURL: URL
+        let fontName: String
+    }
+
     public static var shared = CustomFontLoader()
 
-    private var registeredUrls: Set<URL> = []
+    private var registeredFonts: Set<FontEntry> = []
 
-    public func registerFont(at url: URL) throws {
-        if registeredUrls.contains(url) {
+    public func registerFont(at url: URL, with fontName: String) throws {
+        let entry = FontEntry(fileURL: url, fontName: fontName)
+        if registeredFonts.contains(entry) {
             return
         }
 
         var error: Unmanaged<CFError>?
 
-        CTFontManagerRegisterFontsForURL(url as CFURL, .process, &error)
+        CTFontManagerRegisterFontsForURL(entry.fileURL as CFURL, .process, &error)
 
         if let error = error?.takeRetainedValue() {
             throw error
         }
 
-        registeredUrls.insert(url)
+        registeredFonts.insert(entry)
     }
 }
