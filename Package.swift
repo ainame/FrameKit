5bf35a5826a8869cd406cea8fc5eda007f98bf8b
ainame <s.namai.09@gmail.com>
2022-08-23 14:37:33 +0100

    Add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         .target(
